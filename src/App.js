e0a958973bc800464bb6680037000e2f5d66bde8
anvpham <anvpham4474@gmail.com>
2021-03-21 19:41:41 +0700

    Update UI for array simulator component

--- src/App.js
+++ src/App.js
@@ -37,97 +37,96 @@ function App() {
 
   return (
     <div className={style.app}>
-      <div className={style.header}>
-        <div className={style.typo}>
-          Course: Advanced Data Structures and Algorithms
-        </div>
-        <div className={style.typo}>
-          Topic: B-Tree
-          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-          Nhóm: 3
-        </div>
-      </div>
       <div className={style.body}>
-        <div className={style.treeVisualizer}>
-          <div className={style.treeContainer}>
-            <div className={`${style.layer} ${style.layer1}`}>
-              <div className={style.node}>
-                <NodeValue>55</NodeValue>
+        <div className={style.leftPage}>
+          <div className={style.title}>
+            <div className={style.typo}>
+              Course: Advanced Data Structures and Algorithms
+              <div className={style.typo}>
+                Topic: B-Tree &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Nhóm: 3
               </div>
             </div>
-            <div className={style.layer}>
-              <div className={`${style.node} ${style.nodelayer2}`}>
-                <NodeValue>46</NodeValue>
-                <div className={style.bar}></div>
-                <NodeValue>51</NodeValue>
+          </div>
+          <div className={style.treeVisualizer}>
+            <div className={style.treeContainer}>
+              <div className={`${style.layer} ${style.layer1}`}>
+                <div className={style.node}>
+                  <NodeValue>55</NodeValue>
+                </div>
               </div>
-              <div className={`${style.node} ${style.nodelayer2}`}>
-                <NodeValue>63</NodeValue>
-                <div className={style.bar}></div>
-                <NodeValue>67</NodeValue>
+              <div className={style.layer}>
+                <div className={`${style.node} ${style.nodelayer2}`}>
+                  <NodeValue>46</NodeValue>
+                  <div className={style.bar}></div>
+                  <NodeValue>51</NodeValue>
+                </div>
+                <div className={`${style.node} ${style.nodelayer2}`}>
+                  <NodeValue>63</NodeValue>
+                  <div className={style.bar}></div>
+                  <NodeValue>67</NodeValue>
+                </div>
               </div>
-            </div>
-            <div className={style.layer}>
-              <div className={`${style.node} ${style.nodelayer3}`}>
-                <NodeValue>41</NodeValue>
+              <div className={style.layer}>
+                <div className={`${style.node} ${style.nodelayer3}`}>
+                  <NodeValue>41</NodeValue>
+                </div>
+                <div className={`${style.node} ${style.nodelayer3}`}>
+                  <NodeValue>48</NodeValue>
+                </div>
+                <div className={`${style.node} ${style.nodelayer3}`}>
+                  <NodeValue>53</NodeValue>
+                  <div></div>
+                </div>
+                <div className={`${style.node} ${style.nodelayer3}`}>
+                  <NodeValue>61</NodeValue>
+                </div>
+                <div className={`${style.node} ${style.nodelayer3}`}>
+                  <NodeValue>65</NodeValue>
+                </div>
+                <div className={`${style.node} ${style.nodelayer3}`}>
+                  <NodeValue>69</NodeValue>
+                </div>
               </div>
-              <div className={`${style.node} ${style.nodelayer3}`}>
-                <NodeValue>48</NodeValue>
-              </div>
-              <div className={`${style.node} ${style.nodelayer3}`}>
-                <NodeValue>53</NodeValue>
-                <div></div>
-              </div>
-              <div className={`${style.node} ${style.nodelayer3}`}>
-                <NodeValue>61</NodeValue>
-              </div>
-              <div className={`${style.node} ${style.nodelayer3}`}>
-                <NodeValue>65</NodeValue>
-              </div>
-              <div className={`${style.node} ${style.nodelayer3}`}>
-                <NodeValue>69</NodeValue>
-              </div>
-            </div>
-            <div className={style.layer}>
-              <div className={`${style.node} ${style.nodelayer4}`}>
-                <NodeValue>40</NodeValue>
-              </div>
-              <div className={`${style.node} ${style.nodelayer4}`}>
-                <NodeValue>45</NodeValue>
-              </div>
-              <div className={`${style.node} ${style.nodelayer4}`}>
-                <NodeValue>47</NodeValue>
-              </div>
-              <div className={`${style.node} ${style.nodelayer4}`}>
-                <NodeValue>49</NodeValue>
-                <div className={style.bar}></div>
-                <NodeValue>50</NodeValue>
-              </div>
-              <div className={`${style.node} ${style.nodelayer4}`}>
-                <NodeValue>52</NodeValue>
-              </div>
-              <div className={`${style.node} ${style.nodelayer4}`}>
-                <NodeValue>54</NodeValue>
-              </div>
-              <div className={`${style.node} ${style.nodelayer4}`}>
-                <NodeValue>60</NodeValue>
-              </div>
-              <div className={`${style.node} ${style.nodelayer4}`}>
-                <NodeValue>62</NodeValue>
-              </div>
-              <div className={`${style.node} ${style.nodelayer4}`}>
-                <NodeValue>64</NodeValue>
-              </div>
-              <div className={`${style.node} ${style.nodelayer4}`}>
-                <NodeValue>66</NodeValue>
-                <div className={style.bar}></div>
-                <NodeValue>68</NodeValue>
-              </div>
-              <div className={`${style.node} ${style.nodelayer4}`}>
-                <NodeValue>70</NodeValue>
-                <div className={style.bar}></div>
-                <NodeValue>71</NodeValue>
+              <div className={style.layer}>
+                <div className={`${style.node} ${style.nodelayer4}`}>
+                  <NodeValue>40</NodeValue>
+                </div>
+                <div className={`${style.node} ${style.nodelayer4}`}>
+                  <NodeValue>45</NodeValue>
+                </div>
+                <div className={`${style.node} ${style.nodelayer4}`}>
+                  <NodeValue>47</NodeValue>
+                </div>
+                <div className={`${style.node} ${style.nodelayer4}`}>
+                  <NodeValue>49</NodeValue>
+                  <div className={style.bar}></div>
+                  <NodeValue>50</NodeValue>
+                </div>
+                <div className={`${style.node} ${style.nodelayer4}`}>
+                  <NodeValue>52</NodeValue>
+                </div>
+                <div className={`${style.node} ${style.nodelayer4}`}>
+                  <NodeValue>54</NodeValue>
+                </div>
+                <div className={`${style.node} ${style.nodelayer4}`}>
+                  <NodeValue>60</NodeValue>
+                </div>
+                <div className={`${style.node} ${style.nodelayer4}`}>
+                  <NodeValue>62</NodeValue>
+                </div>
+                <div className={`${style.node} ${style.nodelayer4}`}>
+                  <NodeValue>64</NodeValue>
+                </div>
+                <div className={`${style.node} ${style.nodelayer4}`}>
+                  <NodeValue>66</NodeValue>
+                  <div className={style.bar}></div>
+                  <NodeValue>68</NodeValue>
+                </div>
+                <div className={`${style.node} ${style.nodelayer4}`}>
+                  <NodeValue>70</NodeValue>
+                  <div className={style.bar}></div>
+                  <NodeValue>71</NodeValue>
+                </div>
               </div>
             </div>
           </div>
